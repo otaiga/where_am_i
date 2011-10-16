2d83ce25665eefd49db80e8db8c6cfa67b590b64
Karl Turner <karl.turner5@gmail.com>
2011-10-16 18:29:38 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereAmI::Application.config.secret_token = '50169815cda2271dfd001841f6af378b837b4542914ee2d940f21b374fb6f28b9814741e9ad1c1888712a884e71d6cb6646be39fa04141f8db64240f85de618c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WhereAmI::Application.config.session_store :cookie_store, key: '_where_am_i_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WhereAmI::Application.config.session_store :active_record_store
