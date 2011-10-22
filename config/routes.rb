ee88b3fd5b6c2f821d3bdd599b9ad2705214f9de
Karl Turner <karl.turner5@gmail.com>
2011-10-22 09:45:16 +0100

    added a main controller and sign-in and sign-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WhereAmI::Application.routes.draw do
+  get "main/index"
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ WhereAmI::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
