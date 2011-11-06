6524d3b0db8fd8ee5daa9bc8a208793e6a70bef1
Karl Turner <karl.turner5@gmail.com>
2011-11-06 19:31:18 +0000

    added custom greeting on sign in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,8 @@ class MainController < ApplicationController
   	if user_signed_in?
   	@user = User.find(current_user.id)
   	@whitelist = @user.whitelists.all
+  else
+  	redirect_to "/users/sign_in"
   end
 end
 end
